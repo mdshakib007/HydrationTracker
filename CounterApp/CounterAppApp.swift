eb126049e997150f12a92c38f2059dab202e8dae
mdshakib007 <shakib@mail.com>
2026-01-19 20:52:32 +0600

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by SHAKIB on 19/1/26.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
